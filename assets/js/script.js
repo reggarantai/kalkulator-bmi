ebf618b0ab09656358f103741692949ff7b993e0
regga rantai <reggarantai@reggarantai-MBP.local>
2021-03-14 14:24:34 +0700

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,31 @@
 function hitung(){
   $('#hasil').hide();
 
-  var hasil = 'Kamu <strong>Obesitas</strong>',
-  className = 'bg-danger text-white',
+  var hasil = 'Kamu <strong>Obesitas</strong> tingkat 3',
+  className = 'bg-danger-2 text-white',
   tinggi = $('#tinggi').val() / 100,
   berat = $('#berat').val(),
   bmi = (berat / ( tinggi * tinggi )).toFixed(1);
 
   if(bmi < 18.5){
     hasil = 'Berat badan kamu <strong>Kurang</strong>';
-  }else if(bmi >= 18.5 && bmi < 23){
+    className = 'bg-orange text-white';
+  }
+  if(bmi >= 18.5 && bmi < 23){
     hasil = 'Berat badan kamu <strong>Normal</strong>';
     className = 'bg-success text-white';
-  }else if(bmi >= 23 && bmi < 30){
+  }
+  if(bmi >= 23 && bmi < 30){
     hasil = 'Berat badan kamu <strong>Berlebih</strong>';
     className = 'bg-warning text-dark';
+  }
+  if(bmi >= 30 && bmi < 35){
+    hasil = 'Kamu <strong>Obesitas</strong> tingkat 1';
+    className = 'bg-warning-2 text-white';
+  }
+  if(bmi >= 35 && bmi < 40){
+    hasil = 'Kamu <strong>Obesitas</strong> tingkat 2';
+    className = 'bg-danger text-white';
   }
 
   $('#hasil').html(
